a21f7c0195156bcffa1f8971ea5971a5336dcac9
Parthiba02 <bandyopadhyayparthiba@gmail.com>
2025-07-27 20:13:15 +0530

    Add files via upload

---
+++ Proj_07A_LDA_IRIS_Dataset.py
@@ -0,0 +1,50 @@
+from sklearn import datasets
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+irisset = datasets.load_iris()
+#The Iris dataset was used in R.A. Fisher's classic 1936 paper, 
+#The Use of Multiple Measurements in Taxonomic Problems, 
+#and can also be found on the UCI Machine Learning Repository.
+# It includes three iris species with 50 samples each as well as 
+#some properties about each flower.
+# The 3 species of iris are
+#Iris setosa, Iris virginica and Iris versicolor
+#The columns in this dataset are:    
+#Id
+#SepalLengthCm
+#SepalWidthCm
+#PetalLengthCm
+#PetalWidthCm
+#Species
+X = irisset.data[:100,:]
+y = irisset.target[:100]
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.20, random_state = 5) 
+plt.figure(1);
+plt.scatter(X[:, 0], X[:, 1], c = y)
+plt.suptitle('Original IRIS Data')
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.grid(1,which='both')
+plt.axis('tight')
+plt.show()
+lda = LinearDiscriminantAnalysis()
+lda.fit(Xtrain,ytrain)
+ypred = lda.predict(X)
+plt.figure(1);
+plt.scatter(X[:, 0], X[:, 1], c = ypred)
+plt.suptitle('Predicted IRIS Data')
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.grid(1,which='both')
+plt.axis('tight')
+plt.show()
+cmat = confusion_matrix(y, ypred)
+print('Confusion matrix of LDA is \n',cmat,'\n')
+disp = ConfusionMatrixDisplay(confusion_matrix=cmat)
+disp.plot()
+LDAscore = accuracy_score(y, ypred )
+print('Accuracy score of LDA is',100*LDAscore,'%\n')
